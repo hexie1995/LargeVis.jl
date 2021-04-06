823a5d2122b33670b379fbe80aa12b4fcf297a6d
Documenter.jl <documenter@juliadocs.github.io>
2021-04-06 14:55:59 +0000

    build based on 5ced4af

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#LargeVis","page":"LargeVis","title":"LargeVis","text":"","category":"section"},{"location":"","page":"LargeVis","title":"LargeVis","text":"2d embedding for visualization of high dimensional data while preserving data similarity","category":"page"},{"location":"#Functions","page":"LargeVis","title":"Functions","text":"","category":"section"},{"location":"","page":"LargeVis","title":"LargeVis","text":"","category":"page"}]
+}
